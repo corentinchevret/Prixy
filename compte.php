263ea02b0a6732f2b66a40af27b12ab58d7280a7
Unknown <marwane.berthod@orange.fr>
2016-11-03 17:45:49 +0100

    création fichiers compte+modifications page profil

---
+++ compte.php
@@ -0,0 +1,11 @@
+		<?php  $titre = 'Prixy - Mon Compte'; include("barnav.php"); ?>		
+			<div class="container">
+				<div class="row">
+					<h1><a href="compte.php"><i class="fa fa-user" aria-hidden="true"></i> Mon Compte</a></h1><br>
+				</div>
+
+
+
+
+			</div>
+		<?php include("footer.php"); ?>
